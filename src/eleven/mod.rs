f132108b74cc44b31204922b0a0472002a3752c4
Tillmann Zipperer <tillmann.zipperer@web.de>
2023-12-12 01:06:54 +0100

    Add 11 part 2

--- src/eleven/mod.rs
+++ src/eleven/mod.rs
@@ -1,17 +1,15 @@
 use std::collections::HashSet;
+
 fn expand(mut universe: Vec<Vec<char>>) -> Vec<Vec<char>> {
     let mut rows_to_insert_to: Vec<usize> = vec![];
     let mut cols_to_insert_not_to: Vec<usize> = vec![];
-    let mut rows = universe.len();
-    println!("rows: {rows}");
-    let mut cols = universe[0].len();
-    let mut elems_inserted = 0;
+    let rows = universe.len();
+    let cols = universe[0].len();
     for (r, row) in universe.iter().enumerate() {
         let mut found_hashes = false;
         for elem in row.iter() {
             if elem == &'#' {
                 found_hashes = true;
-                println!("Found hashes on r: {r}");
             }
         }
         if !found_hashes {
@@ -19,21 +17,18 @@ fn expand(mut universe: Vec<Vec<char>>) -> Vec<Vec<char>> {
         }
     }
     for row in &universe[..] {
-        for (i, (j, col)) in row.iter().zip(row.iter().rev()).enumerate() {
+        for (i, (j, _col)) in row.iter().zip(row.iter().rev()).enumerate() {
             if j == &'#' {
                 cols_to_insert_not_to.push(i);
             }
         }
     }
-    println!("Cols to insert not to: {:?}", cols_to_insert_not_to);
     let mut cols_to_insert_to: Vec<usize> = vec![];
     for i in (0..cols).rev() {
-        println!("{}", i);
         if ! cols_to_insert_not_to.contains(&i) {
             cols_to_insert_to.push(i);
         }
     }
-    println!("Cols to insert to: {:?}", cols_to_insert_to);
     // insert cols
     let len_cols = cols_to_insert_to.len();
     for col in cols_to_insert_to {
@@ -42,41 +37,105 @@ fn expand(mut universe: Vec<Vec<char>>) -> Vec<Vec<char>> {
         }
     }
 
-    println!("Rows to insert to: {:?}", rows_to_insert_to);
     for row in rows_to_insert_to.iter().rev() {
         universe.insert(*row, vec!['.';cols+len_cols]);
-    }
-    for row in &universe {
-        println!("{:?}", row);
     }
     universe
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)]
-struct Galaxy {
-    x: i32,
-    y: i32,
+#[derive(Debug)]
+struct Universe {
+    insertions_x: Vec<i128>,
+    insertions_y: Vec<i128>,
+    universe: Vec<Vec<char>>,
 }
 
+impl Universe {
+    fn find_insertion_points_y(&mut self) {
+        for (r, row) in self.universe.iter().enumerate() {
+            let mut found_hashes = false;
+            for elem in row.iter() {
+                if elem == &'#' {
+                    found_hashes = true;
+                }
+            }
+            if !found_hashes {
+                self.insertions_x.push(r as i128);
+            }
+        }
+    }
+    fn find_insertion_points_x(&mut self) {
+        let cols = self.universe[0].len();
+        let mut cols_to_insert_not_to: Vec<usize> = vec![];
+        for row in &self.universe[..] {
+            for (i, (j, _col)) in row.iter().zip(row.iter()).enumerate() {
+                if j == &'#' {
+                    cols_to_insert_not_to.push(i);
+                }
+            }
+        }
+        for i in 0..cols {
+            if ! cols_to_insert_not_to.contains(&i) {
+                self.insertions_y.push(i as i128);
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Ord, PartialOrd)]
+struct Galaxy {
+    id: i32,
+    x: i128,
+    y: i128,
+    insertions_x: Vec<i128>,
+    insertions_y: Vec<i128>,
+    multiplier: i128,
+}
 
 impl Galaxy {
-    fn shortest_path_to(&self, other: &Galaxy) -> i32{
+    fn shortest_path_to(&self, other: &Galaxy) -> i128{
         let x1 = self.x;
         let y1 = self.y;
         let x2 = other.x;
         let y2 = other.y;
-        println!("{}", x1 -x2 );
-        let count = (x1 - x2).abs() + (y1 -y2).abs();
+        let mut insertions_between_x = 0;
+        let mut insertions_between_y = 0;
+        if x1 < x2 {
+            for x in x1..=x2 {
+                if other.insertions_x.contains(&x) {
+                    insertions_between_x += 1;
+                }
+            }
+        } else {
+            for x in x2..=x1 {
+                if self.insertions_x.contains(&x) {
+                    insertions_between_x += 1;
+                }
+            }
+        }
+        if y1 < y2 {
+            for y in y1..=y2 {
+                if other.insertions_y.contains(&y) {
+                    insertions_between_y += 1;
+                }
+            }
+        } else {
+            for y in y2..=y1 {
+                if self.insertions_y.contains(&y) {
+                    insertions_between_y += 1;
+                }
+            }
+        }
+        let count = (x1 - x2).abs() + insertions_between_x  * self.multiplier + (y1 -y2).abs() + insertions_between_y * self.multiplier;
         count
     }
 }
 
-fn solve1(input: &str) -> i32 {
+fn solve1(input: &str) -> i128 {
     let lines = input.lines();
     let mut sum = 0;
     let amount_rows = lines.clone().count();
     let amount_cols = lines.clone().nth(0).unwrap().chars().count();
-    println!("{amount_rows} {amount_cols}");
     let mut universe: Vec<Vec<char>> = vec![vec!['0'; amount_cols]; amount_rows];
     for (i, line) in lines.enumerate() {
         for (j, c) in line.chars().enumerate() {
@@ -86,10 +145,12 @@ fn solve1(input: &str) -> i32 {
     // expand universe
     let universe = expand(universe.clone());
     let mut galaxies: Vec<Galaxy> = vec![];
+    let mut id = 1;
     for (i, line) in universe.iter().enumerate() {
         for (j, c) in line.iter().enumerate() {
             if c == &'#' {
-                galaxies.push(Galaxy { x: i as i32, y: j as i32 });
+                galaxies.push(Galaxy {id: id, x: i as i128, y: j as i128, insertions_x: vec![], insertions_y: vec![], multiplier: 1 });
+                id += 1;
             }
         }
     }
@@ -105,14 +166,63 @@ fn solve1(input: &str) -> i32 {
     sum
 }
 
-fn solve2(input: &str) -> u32 {
-    0
+fn solve2(input: &str, mult: i128) -> i128 {
+    let lines = input.lines();
+    let mut sum = 0;
+    let amount_rows = lines.clone().count();
+    let amount_cols = lines.clone().nth(0).unwrap().chars().count();
+    let mut universe2: Vec<Vec<char>> = vec![vec!['0'; amount_cols]; amount_rows];
+    for (i, line) in lines.enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            universe2[i][j] = c;
+        }
+    }
+    // create universe
+    let mut universe = Universe {
+            insertions_x: vec![],
+            insertions_y: vec![],
+            universe: universe2.clone()
+    };
+    // find insertion points
+    universe.find_insertion_points_x();
+    universe.find_insertion_points_y();
+
+    let mut galaxies: Vec<Galaxy> = vec![];
+    let mut id = 1;
+    for (i, line) in universe2.iter().enumerate() {
+        for (j, c) in line.iter().enumerate() {
+            if c == &'#' {
+                let ins_x: Vec<i128> = universe
+                    .insertions_x
+                    .iter()
+                    .filter_map(|x| if *x < i as i128 { Some(*x) } else { None })
+                    .collect();
+                let ins_y: Vec<i128> = universe
+                    .insertions_y
+                    .iter()
+                    .filter_map(|y| if *y < j as i128 { Some(*y) } else { None })
+                    .collect();
+                galaxies.push(Galaxy {id: id, x: i as i128, y: j as i128, insertions_x: ins_x.clone(), insertions_y: ins_y.clone(), multiplier: mult });
+                id += 1;
+            }
+        }
+    }
+    let mut hashset = HashSet::new();
+    for (i, el1) in galaxies.iter().enumerate() {
+        for (_j, el2) in galaxies[i+1..].iter().enumerate() {
+            hashset.insert((el1, el2));
+        }
+    }
+    for element in hashset.iter() {
+        sum += element.0.shortest_path_to(element.1);
+    }
+    sum
 }
 
 pub fn solve() {
     let input = include_str!("input");
     println!("Solution 1: {}", solve1(input));
-    println!("Solution 2: {}", solve2(input));
+    println!("Solution 2: {}", solve2(input, 999999));
 }
 
 #[cfg(test)]
@@ -143,6 +253,7 @@ mod tests {
 .........#...
 #....#.......";
 
+
     #[test]
     fn one_solved() {
         let res = solve1(SAMPLE);
@@ -150,9 +261,14 @@ mod tests {
     }
 
     #[test]
-    fn two_solved() {
-        let res = solve2(SAMPLE);
-        assert_eq!(res, 71503);
+    fn two_solved_10() {
+        let res = solve2(SAMPLE, 9);
+        assert_eq!(res, 1030);
+    }
+    #[test]
+    fn two_solved_100() {
+        let res = solve2(SAMPLE, 99);
+        assert_eq!(res, 8410);
     }
 
     #[test]
