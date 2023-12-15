78086810db731391cbee301c71ece122786fa889
Tillmann Severin <tillmann.severin@ul.com>
2023-12-15 19:55:34 +0100

    Working Solution 2, nice function in num!

--- src/eight/mod.rs
+++ src/eight/mod.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 use std::collections::VecDeque;
 use regex::Regex;
+use num::integer::lcm;
 
-fn solve1(input: &str) -> u32 {
+fn solve1(input: &str) -> u128 {
     let mut hashmap: HashMap<&str, (&str, &str)> = HashMap::new();
     let split_lines: Vec<&str> = input.split("\n\n").collect();
     // Pattern is the first line
@@ -41,55 +42,72 @@ fn solve1(input: &str) -> u32 {
     sum
 }
 
-fn solve2(input: &str) -> u32 {
+#[allow(dead_code)]
+fn parse_input(input: &str) -> (VecDeque<char>, HashMap<&str, (&str, &str)>) {
     let mut hashmap: HashMap<&str, (&str, &str)> = HashMap::new();
     let split_lines: Vec<&str> = input.split("\n\n").collect();
     // Pattern is the first line
     let char_vec: Vec<char> = split_lines[0].chars().collect();
-    let mut pattern = VecDeque::from(char_vec);
+    let pattern = VecDeque::from(char_vec);
+    let node_re = Regex::new(r"(?<node_name>\w{3}) = \((?<left>\w{3}), (?<right>\w{3})\)").unwrap();
+    for line in split_lines[1].lines() {
+        let caps = node_re.captures(line).unwrap();
+        hashmap.insert(caps.name("node_name").unwrap().as_str(),(caps.name("left").unwrap().as_str(), caps.name("right").unwrap().as_str()));
+    }
+    (pattern, hashmap)
+}
+
+fn get_loop_size(mut pattern: VecDeque<char>, hashmap: HashMap<&str, (&str, &str)>, start_node: &str) -> u128 {
+    let mut sum = 1;
+    let mut next = hashmap.get_key_value(start_node).unwrap();
+    if pattern.front() == Some(&'L') {
+        next = hashmap.get_key_value(next.1.0).unwrap();
+    } else {
+        next = hashmap.get_key_value(next.1.1).unwrap();
+    }
+    pattern.rotate_left(1);
+    while next.0 != &start_node {
+        if pattern.front() == Some(&'L') {
+            next = hashmap.get_key_value(next.1.0).unwrap();
+        } else {
+            next = hashmap.get_key_value(next.1.1).unwrap();
+        }
+        sum += 1;
+        pattern.rotate_left(1);
+    }
+    sum
+}
+
+fn solve2(input: &str) -> u128 {
+    let mut hashmap: HashMap<&str, (&str, &str)> = HashMap::new();
+    let split_lines: Vec<&str> = input.split("\n\n").collect();
+    // Pattern is the first line
+    let char_vec: Vec<char> = split_lines[0].chars().collect();
+    let pattern = VecDeque::from(char_vec);
     // println!("{pattern:?}");
     // Afterwards only nodes follow
     let node_re = Regex::new(r"(?<node_name>\w{3}) = \((?<left>\w{3}), (?<right>\w{3})\)").unwrap();
     let mut starting_points = vec![];
+    let mut end_points = vec![];
     for line in split_lines[1].lines() {
         let caps = node_re.captures(line).unwrap();
         hashmap.insert(caps.name("node_name").unwrap().as_str(),(caps.name("left").unwrap().as_str(), caps.name("right").unwrap().as_str()));
         if caps.name("node_name").unwrap().as_str().ends_with("A") {
             starting_points.push(caps.name("node_name").unwrap().as_str());
         }
-    }
-    // println!("{:?}", starting_points);
-    let mut sum = 0;
-    let mut rotate_further = true;
-    let mut next = vec![];
-    for start in starting_points {
-        next.push(hashmap.get_key_value(start).unwrap().clone());
-    }
-    while rotate_further {
-        // println!("{:?} {:?}", pattern.front(), next);
-        for nnext in next.iter_mut() {
-            if pattern.front() == Some(&'L') {
-                // left
-                *nnext = hashmap.get_key_value(nnext.1.0).unwrap();
-            } else if pattern.front() == Some(&'R') {
-                // right
-                *nnext = hashmap.get_key_value(nnext.1.1).unwrap();
-            } else {
-                panic!();
-            }
+        if caps.name("node_name").unwrap().as_str().ends_with("Z") {
+            end_points.push(caps.name("node_name").unwrap().as_str());
         }
-        sum += 1;
-        rotate_further = false;
-        // println!("{:?}", next);
-        for nnext in next.iter_mut() {
-            if ! nnext.0.ends_with("Z") {
-                rotate_further = true;
-                break;
-            }
-        }
-        pattern.rotate_left(1);
     }
-    //println!("{hashmap:?}");
+    let mut loop_sizes = vec![];
+    for end in end_points {
+        let loop_size = get_loop_size(pattern.clone(), hashmap.clone(), end);
+        loop_sizes.push(loop_size);
+    }
+    let mut sum = loop_sizes[0];
+    for elem in &loop_sizes[1..] {
+        sum = lcm(sum, *elem);
+    }
     sum
 }
 
@@ -143,6 +161,19 @@ XXX = (XXX, XXX)";
 
     #[test]
     fn test_two_solved() {
-        assert_eq!(7, solve2(SAMPLE3));
+        assert_eq!(6, solve2(SAMPLE3));
+    }
+
+    #[test]
+    fn test_loop_size() {
+        let (pattern, hashmap) = parse_input(SAMPLE3);
+        let loop_size = get_loop_size(pattern, hashmap, "11Z");
+        assert_eq!(2, loop_size);
+    }
+    #[test]
+    fn test_loop_size_2() {
+        let (pattern, hashmap) = parse_input(SAMPLE3);
+        let loop_size = get_loop_size(pattern, hashmap, "22Z");
+        assert_eq!(3, loop_size);
     }
 }
